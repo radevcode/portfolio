323f7516a13616948b494970b80f488b633f84c0
radevcode <radevcode@gmail.com>
2026-01-11 16:25:44 -0300

    Connection with the GITHUB API OK!

--- backend/main.py
+++ backend/main.py
@@ -6,35 +6,52 @@ import os
 app = FastAPI()
 
 #Configure CORS so that React can comumnicate with the backend
-app.add_middelware(
+app.add_middleware(
     CORSMiddleware,
     allow_origins=["*"], #For production write the real url
     allow_methods=["*"],
 )
     
-env_users = os.getenv("GITHUB_USERS", "racode75")
-GITHUB_USERS = env_users.split(",")
+env_users = os.getenv("GITHUB_USERS", "")
+#Detect if the env file is not load...
+if not env_users:
+    print("Warning: the users did not load from the .env file")
+    GITHUB_USERS=["racode75"]
+else:
+    GITHUB_USERS = [u.strip() for u in env_users.split(",") if u.strip()]#env_users.split(",")
+
 GITHUB_TOKEN = os.getenv("GITHUB_TOKEN")
+#debug
+@app.get("/test")
+def test():
+    return {"status": "ok", "usuarios": GITHUB_USERS}
+
 
 @app.get("/api/github/repos")
 async def get_github_repos():
-    url = f"https://api.github.com/users/{GITHUB_USERS}/repos?sort=updated"
+    print(f"DEBUG: Usuarios cargados: {GITHUB_USERS}")
+    all_repos = []
     headers = {"Authorization": f"token {GITHUB_TOKEN}"} if GITHUB_TOKEN else {}
 
     async with httpx.AsyncClient() as client:
-        response = await client.get(url, headers=headers)
-        if response.status_code != 200:
-            raise HTTPException(status_code=response.status_code, detail="Error con GitHub")
-        
-        #Filtering...
-        repos = response.json()
-        return [
-            {
-                "name": r["name"],
-                "url": r["html_url"],
-                "description": r["description"],
-                "stars": r["stargazers_count"],
-                "language": r["language"]
-            }
-            for r in repos if not r["fork"] # Only my projects
-        ]
+        for user in GITHUB_USERS:
+            user = user.strip() # clean whitespaces
+            url = f"https://api.github.com/users/{user}/repos?sort=updated"
+            response = await client.get(url, headers=headers)
+            if response.status_code == 200:
+                        #Filtering...
+                repos_data = response.json()
+
+                for r in repos_data:
+                    if not r["fork"]:
+                        all_repos.append({
+                            "name": r["name"],
+                            "url": r["html_url"],
+                            "description": r["description"],
+                            "stars": r["stargazers_count"],
+                            "language": r["language"]
+                        }) 
+            else:
+                print(f"Error consultando a {user}: {response.status_code}")
+    return all_repos                             
+
